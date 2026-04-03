d85665de486db1565b8bb834ac02060344f18b9f
Ranzlappen <107426883+Ranzlappen@users.noreply.github.com>
2026-04-03 09:40:20 +0200

    Fix import path for firebase module

--- polyvote/src/main.tsx
+++ polyvote/src/main.tsx
@@ -8,7 +8,7 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import './index.css';
-import './lib/firebase';
+import './firebase';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
